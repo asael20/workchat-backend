7a3ac0f7b4b030fd92bfab977dd5f158965384aa
Asael de la Rosa Sampayo <asaelrs20@gmail.com>
2021-06-23 22:19:37 -0500

    cambio del nombre de algunas variables

--- src/resources/relation.resources.js
+++ src/resources/relation.resources.js
@@ -6,8 +6,8 @@ const { _relationService }  = require('../core/services');
 
 relationResource.post('/', async (req, res, next) => {
 
-    const {email, userReceive} = req.body;
-    const result = await _relationService.createRelation({userAsk: email, userReceive})
+    const {userEmailReceive, userEmailSend} = req.body;
+    const result = await _relationService.createRelation({userAsk: userEmailSend, userReceive:userEmailReceive})
     res.status(result.code).json(result)
 });
 
@@ -16,8 +16,8 @@ relationResource.post('/', async (req, res, next) => {
 relationResource.put('/:id', async (req, res, next) => {
 
     const {id} = req.params;
-    const {decision, email} = req.body;
-    const result = await _relationService.changeRelationStatus({contactRequestId: id, userEmail: email, decision})
+    const {decision, userEmail} = req.body;
+    const result = await _relationService.changeRelationStatus({contactRequestId: id, userEmail, decision})
     res.status(result.code).json(result)
 });
 
